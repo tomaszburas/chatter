749f1104e394a224e8e33a41037131956de94630
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-01-13 16:35:14 +0100

    Load-page init

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,18 @@
 const {Router} = require('express');
+const {join} = require('path');
 
 const homeRouter = Router();
 
 homeRouter
-    .get('/', (req, res) => {
-
+    .get('/login', (req, res) => {
+        res.sendFile('login.html', {
+            root: join(__dirname, '../public/html/')
+        })
+    })
+    .get('/register', (req, res) => {
+        res.sendFile('register.html', {
+            root: join(__dirname, '../public/html/')
+        })
     })
 
 module.exports = homeRouter;
